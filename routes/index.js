3b482ef28cfa9063185d8d4d1c002af9af9770e3
adlermo <adlerorteney@gmail.com>
2025-10-09 22:01:53 -0300

    Improve home route message for better understanding

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,16 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
   if (!req.user && !req.session.user)
     return res.send(`
-      <h1>Welcome to Nature Notes API ⛺</h1>
-      <p><a href="/login">Log in with Google 🔓</a></p>
+      <h1>⛺ Welcome to Nature Notes API</h1>
+      <p><a href="/api-docs">🔓 API Documentation</a> <sup>*Note that you'll only have read access to resources (i.e., only GET routes will work)</sup></p>
+      <p><a href="/login">Log in with Google</a></p>
   `);
 
   res.send(`
-    <h1>Welcome to Nature Notes API ⛺</h1>
+    <h1>⛺ Welcome to Nature Notes API</h1>
+    <p><a href="/api-docs">🔐 API Documentation</a> <sup>*You have full access now! Be wise</sup></p>
+    <p><a href="/logout">Log out</a></p>
     <p>Hello, ${req.user?.displayName || req.session.user.displayName}!</p>
-    <p><a href="/api-docs">API Documentation</a></p>
-    <p><a href="/logout">Log out 🔐</a></p>
   `);
 });
 
