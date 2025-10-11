11f4b4bfefac1e287bc1a4683d44a7717617111d
adlermo <adlerorteney@gmail.com>
2025-10-10 22:07:37 -0300

    Add dynamic callbackURL

--- config/google-oauth.js
+++ config/google-oauth.js
@@ -21,7 +21,7 @@ router.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }, (accessToken, refreshToken, profile, done) => {
   mongodb.getDb().collection('users').updateOne(
     { googleId: profile.id },
